825dfc1a4c75d0e48862a9a30bfa07b3bdb6db20
Ojas Sanghi <osanghi@mit.edu>
2026-02-03 12:21:20 -0500

    add generated configs

---
+++ core/configs/gen_feb0126_graphconv_task31/gen_feb0126_graphconv_task31_95.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#SBATCH --time=144:00:00
+#SBATCH -p cpu-gpu-v100
+#SBATCH --gpus=1
+
+uv run python main.py --config gen_feb0126_graphconv_task31/gen_feb0126_graphconv_task31_95
---
+++ core/configs/gen_feb0126_graphconv_task33/gen_feb0126_graphconv_task33_20.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#SBATCH --time=144:00:00
+#SBATCH -p cpu-gpu-v100
+#SBATCH --gpus=1
+
+uv run python main.py --config gen_feb0126_graphconv_task33/gen_feb0126_graphconv_task33_20
