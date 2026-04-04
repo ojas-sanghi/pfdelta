c550e8dc4bafce578fd2a9fb8530eecccba9661d
Ojas Sanghi <osanghi@mit.edu>
2026-04-03 20:54:43 -0400

    config updates

---
+++ core/configs/gen_apr0326_canos_task31/gen_apr0326_canos_task31_11.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH -p mit_normal_gpu,mit_preemptable
+#SBATCH --requeue
+#SBATCH --mem=64G
+#SBATCH -G 1
+#SBATCH --output=canos_%j.out
+#SBATCH --signal=USR1@90
+#SBATCH --time=05:59:00
+
+source ~/.bashrc
+uv run python main.py --config gen_apr0326_canos_task31/gen_apr0326_canos_task31_11
