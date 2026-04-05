d45cc0c0e6833ce19d0d5fa20e6ab8f286f1df31
Ojas Sanghi <osanghi@mit.edu>
2026-04-05 15:38:53 -0400

    reorganize output directories; create and run pfnet and gc on pbl

---
+++ core/configs/gen_pbl_apr0326_powerflownet_task34/gen_pbl_apr0326_powerflownet_task34_0.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -p mit_normal_gpu,mit_preemptable
+#SBATCH --exclude=node1928,node4007
+#SBATCH --requeue
+#SBATCH --mem=64G
+#SBATCH -G 1
+#SBATCH --output=powerflownet_pbl_%j.out
+#SBATCH --signal=USR1@90
+#SBATCH --time=05:59:00
+
+source ~/.bashrc
+uv run python main.py --config gen_pbl_apr0326_powerflownet_task34/gen_pbl_apr0326_powerflownet_task34_0
